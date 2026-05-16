fefe03207bacd9af079f6655ab489c0e2a98aa12
Yawei Hsu <yaweihsu0201@gmail.com>
2026-05-17 06:43:02 +0800

    Add Comments

--- utilities/ellipse_matching.py
+++ utilities/ellipse_matching.py
@@ -1,9 +1,22 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 def draw_ellipse(img, mean, a, b, eigvecs, color=(0,255,0), thickness=2):
+    """
+    Draw an ellipse on the image based on the given parameters.
 
+    Args:
+        img: Input image on which to draw the ellipse.
+        mean: Center of the ellipse (x_mean, y_mean).
+        a: Semi-major axis length.
+        b: Semi-minor axis length.
+        eigvecs: Eigenvectors representing the orientation of the ellipse.
+        color: Color of the ellipse (default is green).
+        thickness: Thickness of the ellipse outline (default is 2).
+
+    Returns:
+        Image with the drawn ellipse.
+    """
     t = np.linspace(0, 2*np.pi, 200)
 
     ellipse_local = np.column_stack((a*np.cos(t), b*np.sin(t)))
@@ -18,48 +31,46 @@ def draw_ellipse(img, mean, a, b, eigvecs, color=(0,255,0), thickness=2):
     cv2.polylines(img, [pts], True, color, thickness)
 
 def ellipse_matching(img_binary):
+    """
+    detect connected components in the binary image, fit an ellipse to each component, and filter based on shape criteria.
+
+    Args:
+        img_binary: Input binary image.
+
+    Returns:
+        List of detected ellipses and all points.
+    """
     num_labels, labels = cv2.connectedComponents(img_binary)
-    #print("Objects found:", num_labels - 1) 
 
     results = []
     all_points = []
 
     for label_id in range(1, num_labels):
         ys, xs = np.where(labels == label_id)
-    #   mean_x = xs.mean()
-    #   mean_y = ys.mean()
         if len(xs) == 0:
                 continue
         
         points = np.column_stack((xs, ys))
-        mean = points.mean(axis=0)   # [x_mean, y_mean]
+        mean = points.mean(axis=0) 
         Z = points - mean
 
         ZT_Z = Z.T @ Z
         eigvals, eigvecs = np.linalg.eig(ZT_Z)
 
         idx = np.argsort(eigvals)[::-1]
-        eigvals = eigvals[idx]
-        eigvecs = eigvecs[:, idx]
+        eigvals, eigvecs = eigvals[idx], eigvecs[:, idx]
 
         ZE = Z @ eigvecs
 
-        x1 = ZE[:,0]
-        x2 = ZE[:,1]
+        x1,x2 = ZE[:,0], ZE[:,1]
 
-        m11 = np.mean(np.abs(x1))
-        m12 = np.mean(np.abs(x2))
+        m11,m12 = np.mean(np.abs(x1)), np.mean(np.abs(x2))
+        m21,m22 = np.mean(x1**2), np.mean(x2**2)
 
-        m21 = np.mean(x1**2)
-        m22 = np.mean(x2**2)
+        a1,a2 = 3*np.pi*m11/4, np.sqrt(4*m21)
+        b1,b2 = 3*np.pi*m12/4, np.sqrt(4*m22)
 
-        a1 = 3*np.pi*m11/4
-        a2 = np.sqrt(4*m21)
-        b1 = 3*np.pi*m12/4
-        b2 = np.sqrt(4*m22)
-
-        a = (a1+a2)/2
-        b = (b1+b2)/2
+        a,b = (a1+a2)/2, (b1+b2)/2
 
         if a <= 0 or b <= 0:
             continue
